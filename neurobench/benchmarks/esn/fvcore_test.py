b03b4847f6e897b2e393cc6ffcba6875f2206427
Jason Yik <jyik@g.harvard.edu>
2023-08-02 15:45:16 -0400

    update op count tests with snn frameworks

---
+++ neurobench/benchmarks/esn/fvcore_test.py
@@ -0,0 +1,92 @@
+import torch.nn as nn
+import torch
+
+from fvcore.nn import FlopCountAnalysis, flop_count_table
+
+class TestModel(nn.Module):
+    '''
+    standard ANN, this works fine
+    '''
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(in_features=1000, out_features=10)
+        self.conv = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=1)
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.act(x)
+        x = x.flatten(1)
+        out = self.fc(x)
+
+        return out
+
+class TestSNNModel(nn.Module):
+    '''
+    Default SNNTorch Leaky layer is not supported by the fvcore tool.
+
+        File "/Users/jasonyik/.pyenv/versions/3.8.10/lib/python3.8/site-packages/snntorch/_neurons/leaky.py", line 197, in forward
+            self.reset = self.mem_reset(self.mem)
+        File "/Users/jasonyik/.pyenv/versions/3.8.10/lib/python3.8/site-packages/snntorch/_neurons/neurons.py", line 106, in mem_reset
+            mem_shift = mem - self.threshold
+        RuntimeError: Cannot insert a Tensor that requires grad as a constant. Consider making it a parameter or input, or detaching the gradient
+    '''
+
+    def __init__(self):
+        super().__init__()
+
+        import snntorch as snn
+        from snntorch import surrogate
+
+        beta = 0.9
+        spike_grad = surrogate.fast_sigmoid()
+        self.net = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(20, 256),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+            nn.Linear(256, 256),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+            nn.Linear(256, 256),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+            nn.Linear(256, 35),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class TestSJModel(nn.Module):
+    '''
+    Default SJ model also fails fvcore
+
+        File "/Users/jasonyik/.pyenv/versions/3.8.10/lib/python3.8/site-packages/spikingjelly/activation_based/neuron.py", line 239, in single_step_forward
+            self.neuronal_charge(x)
+        File "/Users/jasonyik/.pyenv/versions/3.8.10/lib/python3.8/site-packages/spikingjelly/activation_based/neuron.py", line 729, in neuronal_charge
+            self.v = self.neuronal_charge_decay_input_reset0(x, self.v, self.tau)
+        RuntimeError: Cannot insert a Tensor that requires grad as a constant. Consider making it a parameter or input, or detaching the gradient
+    '''
+    def __init__(self):
+        super().__init__()
+
+        from spikingjelly.activation_based import neuron, layer, surrogate
+
+        self.net = nn.Sequential(
+            layer.Flatten(),
+            layer.Linear(28 * 28, 10, bias=False),
+            neuron.LIFNode(tau=2.0, surrogate_function=surrogate.ATan())
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+def test(model, inputs):
+    y = model(inputs)
+    flops = FlopCountAnalysis(model, (inputs,))
+
+    print(flop_count_table(flops))
+
+test(TestModel(), torch.randn((1,3,10,10)))
+
+# test(TestSNNModel(), torch.randn((1,20)))
+
+# test(TestSJModel(), torch.randn((1,28,28)))

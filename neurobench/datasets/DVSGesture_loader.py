86946aa384dec9d4e04a9f271d9f5ec644d4394b
korneelf1 <korneel.vandenberghe@hotmail.be>
2023-08-15 12:27:25 -0400

    Dataloader with 1.7 sec samples (shortest sample constraint)

--- neurobench/datasets/DVSGesture_loader.py
+++ neurobench/datasets/DVSGesture_loader.py
@@ -1,69 +1,71 @@
-"""
-"""
+# Copyright
+# LICENSE
+#
+#
 
+"""
+DVSGesture_loader.py
+------------------------------------------------------------
+DVS Gesture dataset
+
+
+References
+~~~~~~~~~~
+https://research.ibm.com/interactive/dvsgesture/
+
+Author
+~~~~~~
+Korneel Van den Berghe
+
+"""
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 
 from tonic.datasets import DVSGesture as tonic_DVSGesture
 
-from glob import glob
+# from glob import glob
 
 from dataset import NeuroBenchDataset
 
 import os
-import stat
 import numpy as np
-import struct
 import matplotlib.pyplot as plt
+
 # make animation
 from matplotlib.animation import FuncAnimation
 
+
 class DVSGesture(NeuroBenchDataset):
-    '''
-    Requires python 3.10 (introduced root_dir for glob function)
-    Installs DVSGesture Dataset with individual events in each file if not yet installed, else pass path of tonic DVSGesture install
+    """
+    Installs DVSGesture Dataset with individual events in each file,
+    if not yet installed, else pass path of tonic DVSGesture install
+
+    Data information:
+    event rate: 1MHz -> dt 1e-6
+    sample length: 1.7 seconds
+    default timestep for frames: 5 ms
+
+    For possible preprocessing functions see:
     https://docs.prophesee.ai/stable/tutorials/ml/data_processing/event_preprocessing.html?highlight=metavision_ml%20preprocessing
-    event rate: 100K -> dt 1e-5
-    '''
-    def __init__(self, path, split='testing', data_type = 'frames', preprocessing = 'stack'):
-        if split == 'training':
+    """
+
+    def __init__(
+        self, path, split="testing", data_type="frames", preprocessing="stack"
+    ):
+        if split == "training":
             self.dataset = tonic_DVSGesture(save_to=path)
         else:
-            self.dataset = tonic_DVSGesture(save_to=path, train = False)
+            self.dataset = tonic_DVSGesture(save_to=path, train=False)
 
         self.filenames = self.dataset.data
-        self.path      = path
-        self.prepr     = preprocessing
+        self.path = path
+        self.prepr = preprocessing
         self.data_type = data_type
 
         # sample parameters:
-        self._deltat = 5000 # DVS is in microseconds -> deltat = 5ms
-        self._T      = 1000 # in ms
-        self.random_window = True
-        self.longest = 0
-        self.shortest = 1e9
-        # if split == "testing":
-        #     if not installed:
-        #         self.dataset = tonic_DVSGesture(save_to=path, train=False)
-
-        #     self.filenames = []
-        #     for path, subdirs, files in os.walk(path+'\\ibmGestureTest'):
-        #         for name in files:
-        #             self.filenames.append(os.path.join(path,name))
-
-        # elif split == "training":
-        #     if not installed:
-        #         self.dataset = tonic_DVSGesture(save_to=path)
-
-        #     self.filenames = []
-        #     for path, subdirs, files in os.walk(path+'\\ibmGestureTraining'):
-        #         for name in files:
-        #             self.filenames.append(os.path.join(path,name))
-
-        # self.filenames = sorted(list(self.filenames))
-        # self.path = path
-        # self.labels = sorted(glob("*/", root_dir=path))
-        # self.labels = {key[:-1]: idx for idx, key in enumerate(self.labels)}
+        self._deltat = 5000  # DVS is in microseconds -> deltat = 5ms
+        self._T = 1700  # in ms, sample time is 1.7 sec
+        self.random_window = False
 
     def __len__(self):
         return len(self.filenames)
@@ -71,187 +73,165 @@ class DVSGesture(NeuroBenchDataset):
     def __getitem__(self, idx):
         structured_array = self.dataset[idx][0]
 
-        x_data = np.array(structured_array['x'], dtype = np.int16)
-        y_data = np.array(structured_array['y'], dtype = np.int16)
-        p_data = np.array(structured_array['p'], dtype = bool)
-        t_data = np.array(structured_array['t'], dtype = np.int64) # time is in microseconds
+        x_data = np.array(structured_array["x"], dtype=np.int16)
+        y_data = np.array(structured_array["y"], dtype=np.int16)
+        p_data = np.array(structured_array["p"], dtype=bool)
+        t_data = np.array(
+            structured_array["t"], dtype=np.int64
+        )  # time is in microseconds
 
-        t_final = t_data[-1]
-        self.longest = max(t_final, self.longest)
-        self.shortest = min(t_final, self.shortest)
-        xypt = torch.stack((torch.tensor(x_data), torch.tensor(y_data), torch.tensor(p_data), torch.tensor(t_data)),dim = 1)
+        xypt = torch.stack(
+            (
+                torch.tensor(x_data),
+                torch.tensor(y_data),
+                torch.tensor(p_data),
+                torch.tensor(t_data),
+            ),
+            dim=1,
+        )
 
         # create sample
-        t_end = t_data[-1] - self._T*1000 * 1.5 # find latest time at which we can sample including buffer of factor 1.5 (*1000 to convert to microseconds)
-        start_time = np.random.randint(0,t_end) if self.random_window else 0
-        sample = xypt[(start_time<=xypt[:,3])&(xypt[:,3]<=(start_time+self._T*1000))]
-        sample[:,3] = sample[:,3]-sample[0,3] # normalize time again
-        tbins = self._T*1000//self._deltat
-        if self.data_type == 'frames':
-            if self.prepr == 'histo':
-                events = histogram_preprocessing(sample,delta_t = self._deltat,h_og = 128, w_og = 128, display_frame=False)
+        t_end = (
+            t_data[-1] - self._T * 1000
+        )  # find latest time at which we can sample including buffer of factor 1.5 (*1000 to convert to microseconds)
+        start_time = np.random.randint(0, t_end) if self.random_window else 0
+        sample = xypt[
+            (start_time <= xypt[:, 3]) & (xypt[:, 3] <= (start_time + self._T * 1000))
+        ]
+        sample[:, 3] = sample[:, 3] - sample[0, 3]  # shift timestamps
+        tbins = self._T * 1000 // self._deltat
+        if self.data_type == "frames":
+            # add own preprocessing functions
+            if self.prepr == "histo_diff":
+                events = histogram_difference_preprocessing(
+                    sample,
+                    tbins=tbins,
+                    delta_t=self._deltat,
+                    h_og=128,
+                    w_og=128,
+                    display_frame=False,
+                )
                 return events, self.dataset[idx][1]
-            
-            elif self.prepr == 'stack':
-                events = stack_preprocessing(sample,delta_t = self._deltat, tbins = tbins, h_og = 128, w_og = 128, display_frame=False)
+
+            elif self.prepr == "stack":
+                events = stack_preprocessing(
+                    sample,
+                    delta_t=self._deltat,
+                    tbins=tbins,
+                    h_og=128,
+                    w_og=128,
+                    display_frame=False,
+                )
                 return events, self.dataset[idx][1]
-     
+
         return sample, self.dataset[idx][1]
-    
-    def set_sample_params(self, delta_t = 5, length = 1000, random_window = True):
-        '''Enter delta t and sample length in ms
-        If random_window is True, sample will be random timewindow of length within the gesture'''
-        self._deltat       = delta_t*1000 # convert to microseconds
-        self._T            = length
+
+    def set_sample_params(self, delta_t=5, length=1700, random_window=True):
+        """Enter delta t and sample length in ms
+        If random_window is True, sample will be random timewindow of length within the gesture
+        """
+        self._deltat = delta_t * 1000  # convert to microseconds
+        self._T = length
         self.random_window = random_window
-        
-def stack_preprocessing(xypt, delta_t = 5000,tbins = 200, h_og = 128, w_og = 128,channels = 3, display_frame = False):
-    # tbins = xypt[-1,3]//delta_t
-    tbins = tbins
-    # print(tbins, xypt[-1,3])
 
+
+def stack_preprocessing(
+    xypt, delta_t=5000, tbins=200, h_og=128, w_og=128, channels=3, display_frame=False
+):
     frames = np.zeros((tbins, channels, h_og, w_og))
-    for bin, frame in enumerate(frames):
+    for frame in frames:
         # delete prev neg times
-        xypt_new = xypt[xypt[:,3]>=0]
+        xypt_new = xypt[xypt[:, 3] >= 0]
         xypt = xypt_new
-        # print(frame.shape)
-        # change timestamps
-        xypt[:,3] = xypt[:,3] - delta_t
-        # print(xypt[0,3],  xypt[0,3] <=0)
-        xypt_sub = xypt[xypt[:,3]<=0] # events for the current frame
-        pos_pol  = np.unique(xypt_sub[xypt_sub[:,2]==True][:,:2], axis = 0)
-        neg_pol  = np.unique(xypt_sub[xypt_sub[:,2]==False][:,:2], axis = 0)
 
-        frame[0,:,:][pos_pol[:, 0], pos_pol[:, 1]] = 1
-        frame[1,:,:][neg_pol[:, 0], neg_pol[:, 1]] = 1
-        
-            
+        # change timestamps
+        xypt[:, 3] = xypt[:, 3] - delta_t
+
+        xypt_sub = xypt[xypt[:, 3] <= 0]  # events for the current frame
+        pos_pol = np.unique(xypt_sub[xypt_sub[:, 2] == True][:, :2], axis=0)
+        neg_pol = np.unique(xypt_sub[xypt_sub[:, 2] == False][:, :2], axis=0)
+
+        frame[0, :, :][pos_pol[:, 0], pos_pol[:, 1]] = 1
+        frame[1, :, :][neg_pol[:, 0], neg_pol[:, 1]] = 1
 
     if display_frame:
-        # frame = frame/np.max(frame)
+        frame = frame.astype(float) / np.max(frame) 
 
-        animation = FuncAnimation(fig, update, frames=tbins,fargs=(frames,), interval=5)  # Adjust the interval as needed (in milliseconds)
-        animation.save('test.gif')
+        animation = FuncAnimation(
+            fig, update, frames=tbins, fargs=(frames,), interval=delta_t/1000
+        )  
+        animation.save("test.gif")
+        plt.suptitle('Stack preprocessing')
         plt.show()
-        
+
     return frames
 
-def histogram_preprocessing(xypt, delta_t, h_og, w_og,channels = 3, display_frame = False):
-    tbins = xypt[-1,3]//delta_t
-    print(tbins)
+
+def histogram_difference_preprocessing(xypt, delta_t=5000, tbins=200, h_og=128, w_og=128, channels=3, display_frame=False):
     histogram = np.zeros((tbins, channels, h_og, w_og))
-    for bin, frame in enumerate(histogram):
+    for frame in histogram:
         # delete prev neg times
-        xypt_new = xypt[xypt[:,3]>=0]
+        xypt_new = xypt[xypt[:, 3] >= 0]
         xypt = xypt_new
-        # print(frame.shape)
+
         # change timestamps
-        xypt[:,3] = xypt[:,3] - delta_t
-        # print(xypt[0,3],  xypt[0,3] <=0)
-        for i in range(len(xypt)):
-            if xypt[i,3] <=0:
-                if xypt[i,2]==False:
-                    frame[0, xypt[i,0],xypt[i,1]] = xypt[i,2]+255
+        xypt[:, 3] = xypt[:, 3] - delta_t
 
-                else:
-                    frame[1, xypt[i,0],xypt[i,1]] = xypt[i,2]+255
-            
-                # print(xypt[i,2])
-
-            else:
-                # i know this is bad habit, will change later
-                continue
-           
-    if display_frame:
-        frame = frame/np.max(frame)
-        # fig = plt.figure()
-        # fig, ax = plt.subplots()
-        # plt.imshow(frame.transpose(1,2,0))
-        animation = FuncAnimation(fig, update, frames=tbins,fargs=(histogram,), interval=5)  # Adjust the interval as needed (in milliseconds)
-        plt.show()
+        xypt_sub = xypt[xypt[:, 3] <= 0]  # events for the current frame
+        pos_pol, pos_count = np.unique(xypt_sub[xypt_sub[:, 2] == True][:, :2], axis=0, return_counts=True)
+        neg_pol, neg_count = np.unique(xypt_sub[xypt_sub[:, 2] == False][:, :2], axis=0, return_counts=True)
         
+        counts_dict = {}
+
+        # Update counts from the positives
+        for value, count in zip(pos_pol, pos_count):
+            counts_dict[tuple(value)] = counts_dict.get(tuple(value), 0) + count
+
+        # Update counts from the negatives
+        for value, count in zip(neg_pol, neg_count):
+            counts_dict[tuple(value)] = counts_dict.get(tuple(value), 0) - count
+
+        # Convert the dictionary into a NumPy array
+        array_data = [[*key, value] for key, value in counts_dict.items()]
+        result_array = np.array(array_data)
+        pos_pol = result_array[result_array[:,2]>0]
+        neg_pol = result_array[result_array[:,2]<0]
+        frame[0, :, :][pos_pol[:, 0], pos_pol[:, 1]] = pos_pol[:,2]
+        frame[1, :, :][neg_pol[:, 0], neg_pol[:, 1]] = -neg_pol[:,2] # avoid clipping between [0,1]
+
+    if display_frame:
+        frame = frame.astype(float) / np.max(frame) 
+        
+        animation = FuncAnimation(
+            fig, update, frames=tbins, fargs=(histogram,), interval=5
+        )  
+        animation.save("waving_hand.gif", fps=1 / (5e-3))
+        
+        plt.suptitle('Histogram difference method')
+        plt.show()
+
     return histogram
+
+
 fig, ax = plt.subplots()
+
+
 def update(frame, frames):
     ax.clear()
-    image = frames[frame].transpose(1,2,0)
-    # image = image/np.max(image)
-    ax.imshow(image, cmap='brg')  # You can adjust the colormap as needed
-    ax.set_title(f'Frame {frame}')
-# from the spiking jelly github:
+    image = frames[frame].transpose(1, 2, 0)
+
+    ax.imshow(image, cmap="brg")  # You can adjust the colormap as needed
+    ax.set_title(f"Frame {frame}")
 
 
-def load_aedat_v3(file_name: str):
-    '''
-    :param file_name: path of the aedat v3 file
-    :type file_name: str
-    :return: a dict whose keys are ``['t', 'x', 'y', 'p']`` and values are ``numpy.ndarray``
-    :rtype: Dict
-    This function is written by referring to https://gitlab.com/inivation/dv/dv-python . It can be used for DVS128 Gesture.
-    '''
-    with open(file_name, 'rb') as bin_f:
-        # skip ascii header
-        line = bin_f.readline()
-        while line.startswith(b'#'):
-            if line == b'#!END-HEADER\r\n':
-                break
-            else:
-                line = bin_f.readline()
-
-        txyp = {
-            't': [],
-            'x': [],
-            'y': [],
-            'p': []
-        }
-        while True:
-            header = bin_f.read(28)
-            if not header or len(header) == 0:
-                break
-
-            # read header
-            e_type = struct.unpack('H', header[0:2])[0]
-            e_source = struct.unpack('H', header[2:4])[0]
-            e_size = struct.unpack('I', header[4:8])[0]
-            e_offset = struct.unpack('I', header[8:12])[0]
-            e_tsoverflow = struct.unpack('I', header[12:16])[0]
-            e_capacity = struct.unpack('I', header[16:20])[0]
-            e_number = struct.unpack('I', header[20:24])[0]
-            e_valid = struct.unpack('I', header[24:28])[0]
-
-            data_length = e_capacity * e_size
-            data = bin_f.read(data_length)
-            counter = 0
-
-            if e_type == 1:
-                while data[counter:counter + e_size]:
-                    aer_data = struct.unpack('I', data[counter:counter + 4])[0]
-                    timestamp = struct.unpack('I', data[counter + 4:counter + 8])[0] | e_tsoverflow << 31
-                    x = (aer_data >> 17) & 0x00007FFF
-                    y = (aer_data >> 2) & 0x00007FFF
-                    pol = (aer_data >> 1) & 0x00000001
-                    counter = counter + e_size
-                    txyp['x'].append(x)
-                    txyp['y'].append(y)
-                    txyp['t'].append(timestamp)
-                    txyp['p'].append(pol)
-            else:
-                # non-polarity event packet, not implemented
-                pass
-        txyp['x'] = np.asarray(txyp['x'])
-        txyp['y'] = np.asarray(txyp['y'])
-        txyp['t'] = np.asarray(txyp['t'])
-        txyp['p'] = np.asarray(txyp['p'])
-        return txyp
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     path = os.curdir
-    dataset = DVSGesture(os.path.join(path,'neurobench/datasets/DVSGesture'), split = 'train',)
+    dataset = DVSGesture(
+        os.path.join(path, "neurobench/datasets/DVSGesture"),
+        split="testing", preprocessing="histo_diff"
+    )
 
-    print('Infromation of sample')
-    print(dataset.dataset[8][1])
-    gen_test = DataLoader(dataset,batch_size=16,shuffle=True)
-    for local_batch, local_labels in gen_test:
+    print(dataset[8],dataset[8][1])
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    for local_batch, local_labels in dataloader:
         print(local_batch[0].shape, local_labels)
-    print(dataset.shortest, dataset.longest)

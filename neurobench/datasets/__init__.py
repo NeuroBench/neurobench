8c83f64a5e1bab4557636b16effc1351eb8170b8
Jason Yik <jyik@g.harvard.edu>
2023-08-15 16:43:08 -0400

    update dvs gesture example

--- neurobench/datasets/__init__.py
+++ neurobench/datasets/__init__.py
@@ -3,3 +3,4 @@ from .dataset import *
 from .speech_commands import SpeechCommands
 from .primate_reaching import PrimateReaching
 from .megapixel_automotive import MegapixelAutomotive
+from .DVSGesture_loader import DVSGesture

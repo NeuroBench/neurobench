70778bb211d524311a0c7782bbec2a6e785e53d0
noah.pacik-nelson <noah.pacik-nelson@accenture.com>
2023-08-16 08:21:12 -0400

    Initial SNNTorch Framework

--- neurobench/models/snntorch_models.py
+++ neurobench/models/snntorch_models.py
@@ -13,14 +13,12 @@ class SNNTorchModel(NeuroBenchModel):
         self.net = net
         self.net.eval()
 
-    # TODO: Are models responsible for converting their spike trains into a
-    # classification? Or do we leave the output of the models as raw spikes?
     def __call__(self, data):
-        spikes = []
         utils.reset(self.net)
-        for step in range(data.shape[-1]):
-            spk_out, _ = self.net(data[..., step])
+        spikes = []
+        # Data is expected to be shape (batch, timestep, features*)
+        for step in range(data.shape[1]):
+            spk_out, _ = self.net(data[:, step, ...])
             spikes.append(spk_out)
-        spikes = torch.stack(spikes)
-        return spikes.permute(1, 2, 0)
-
+        spikes = torch.stack(spikes).transpose(0, 1)
+        return spikes

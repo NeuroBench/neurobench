379b5a46ac1bd81bca50750707745ced93ad9717
noah.pacik-nelson <noah.pacik-nelson@accenture.com>
2023-08-16 08:48:44 -0400

    Migrate to pytest

--- tests/test_preprocessors/test_speech2spikes.py
+++ tests/test_preprocessors/test_speech2spikes.py
@@ -1,21 +1,19 @@
-import unittest
+from pathlib import Path
+
 import torch
 import torchaudio
 
-from pathlib import Path
 from neurobench.preprocessing.speech2spikes import S2SProcessor
 
-class TestSpeech2Spikes(unittest.TestCase):
-    def setUp(self):
-        sample_file = Path(__file__).parent.joinpath("sample_audio.wav")
-        self.sample_audio, self.sampling_rate = torchaudio.load(sample_file)
+def test_s2s():
+    sample_file = Path(__file__).parent.joinpath("sample_audio.wav")
+    sample_audio, sampling_rate = torchaudio.load(sample_file)
 
-        # Get sample into shape (batch, timestep, features*)
-        self.sample_audio = torch.unsqueeze(self.sample_audio.T, 0)
-        self.sample_audio = torch.tile(self.sample_audio, (100, 1, 1))
+    # Get sample into shape (batch, timestep, features*)
+    sample_audio = torch.unsqueeze(sample_audio.T, 0)
+    sample_audio = torch.tile(sample_audio, (100, 1, 1))
 
-    def test_s2s(self):
-        s2s = S2SProcessor()
-        tensors, targets = s2s((self.sample_audio, torch.Tensor([1]*100)))
-        self.assertTupleEqual(tensors.shape, (100, 60, 20))
-        self.assertTupleEqual(targets.shape, (100,))
+    s2s = S2SProcessor()
+    tensors, targets = s2s((sample_audio, torch.Tensor([1]*100)))
+    assert tensors.shape == (100, 60, 20)
+    assert targets.shape == (100,)

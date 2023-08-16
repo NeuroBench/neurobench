f0ad55f0bbe23c41af02635e756c08572cc44238
noah.pacik-nelson <noah.pacik-nelson@accenture.com>
2023-08-16 08:40:16 -0400

    Updated test cases

--- tests/test_frameworks.py
+++ tests/test_frameworks.py
@@ -1,3 +1,5 @@
+import pytest
+
 import torch
 import snntorch as snn
 
@@ -26,6 +28,15 @@ def test_snntorch_framework():
     # Random data of shape (batch=256, ts=1000, chan=20)
     data = torch.rand((256, 1000, 20))
     spikes = model(data)
-    
     assert data.shape == (256, 1000, 20)
     assert spikes.shape == (256, 1000, 35)
+
+    data = torch.rand((256, 1000, 2, 2, 5))
+    spikes = model(data)
+    assert data.shape == (256, 1000, 2, 2, 5)
+    assert spikes.shape == (256, 1000, 35)
+
+    data = torch.rand((256, 1000, 10, 5))
+    with pytest.raises(RuntimeError, match='mat1 and mat2 shapes cannot be multiplied'):
+        spikes = model(data)
+    

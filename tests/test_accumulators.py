cd18a3752d8ac90136bf39b75538fe7046b291f9
noah.pacik-nelson <noah.pacik-nelson@accenture.com>
2023-08-16 10:24:30 -0400

    Created framework for accumulators

---
+++ tests/test_accumulators.py
@@ -0,0 +1,11 @@
+import torch
+
+from neurobench.accumulators import choose_max_count
+
+def test_choose_max_count():
+    # Create a tensor of all 0's except for one class
+    a = torch.zeros((256, 1000, 35))
+    a[:, :, 5] = 1
+    assert choose_max_count(a).shape == (256, )
+    assert torch.equal(choose_max_count(a), torch.tensor([5] * 256))
+

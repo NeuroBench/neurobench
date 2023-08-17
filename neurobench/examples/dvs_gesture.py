38bbe1d99757c58089d9b7a5bd54785bd702af33
Jason Yik <jyik@g.harvard.edu>
2023-08-17 14:28:22 -0400

    end-to-end dvsg with phony net

--- neurobench/examples/dvs_gesture.py
+++ neurobench/examples/dvs_gesture.py
@@ -7,16 +7,27 @@ from torch import nn
 from snntorch import surrogate
 
 from neurobench.datasets import DVSGesture
-from neurobench.models import SNNTorchModel
+from neurobench.models import TorchModel
 from neurobench.benchmarks import Benchmark
 
 test_set = DVSGesture("data/dvs_gesture/", split="testing", preprocessing="histo_diff")
 test_set_loader = DataLoader(test_set, batch_size=16, shuffle=True)
 
-net = ...
+# useless network always returns 1
+class OnesNet(nn.Module):
+	def __init__(self):
+		super(OnesNet, self).__init__()
+	
+	def forward(self, x):
+		return torch.ones(x.shape[0])
+net = OnesNet()
+
+# for data in test_set_loader:
+# 	print(net(data[0]))
+# 	breakpoint()
 
 ## Define model ##
-model = SNNTorchModel(net)
+model = TorchModel(net)
 
 static_metrics = ["model_size"]
 data_metrics = ["classification_accuracy"]

ab9520ea4aa657b4845a6176a17a29ff79f9000b
Jason Yik <jyik@g.harvard.edu>
2023-08-18 14:15:38 -0400

    add labels as nums

--- neurobench/datasets/speech_commands.py
+++ neurobench/datasets/speech_commands.py
@@ -1,6 +1,8 @@
 """
 """
 import torch
+import os
+from glob import glob
 from torchaudio.datasets import SPEECHCOMMANDS
 from .dataset import NeuroBenchDataset
 
@@ -9,9 +11,13 @@ class SpeechCommands(NeuroBenchDataset, SPEECHCOMMANDS):
     Speech commands dataset v0.02 with 35 keywords
     """
 
-    def __init__(self, subset:str=None, truncate_or_pad_to_1s=True, ):
-        SPEECHCOMMANDS.__init__(self, "./", download=True, subset=subset)
+    def __init__(self, path, subset:str=None, truncate_or_pad_to_1s=True, ):
+        SPEECHCOMMANDS.__init__(self, path, download=True, subset=subset)
         self.truncate_or_pad_to_1s = truncate_or_pad_to_1s
+
+        self.labels = sorted(glob("*/", root_dir=os.path.join(path, "SpeechCommands", "speech_commands_v0.02")))
+        # subtract 1 to account for _background_noise_
+        self.labels = {key[:-1]: idx-1 for idx, key in enumerate(self.labels)}
 
     def __getitem__(self, idx):
         waveform, sample_rate, label, speaker_id, utterance_num =  SPEECHCOMMANDS.__getitem__(self, idx)
@@ -22,11 +28,14 @@ class SpeechCommands(NeuroBenchDataset, SPEECHCOMMANDS):
                 temp_waveform = torch.zeros((sample_rate,))
                 temp_waveform[:waveform.numel()] = waveform
                 waveform = temp_waveform
-        
+
+        waveform = waveform.unsqueeze(-1)
+        label = self.label_to_index(label)
+
         return waveform, label
     
+    def label_to_index(self, label):
+        return torch.tensor(self.labels[label])
+
     def __len__(self):
         return SPEECHCOMMANDS.__len__(self)
-
-
-    

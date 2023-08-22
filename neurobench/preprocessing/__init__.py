1d10df1a5e1f8e7097ae34aafda8715bf63c03ca
noah.pacik-nelson <noah.pacik-nelson@accenture.com>
2023-08-22 18:00:21 -0400

    Switched to lazy importing all modules. Updated dependencies.

--- neurobench/preprocessing/__init__.py
+++ neurobench/preprocessing/__init__.py
@@ -1,3 +1,8 @@
 from .preprocessor import *
+from ..utils import _lazy_import
 
-from .speech2spikes import S2SProcessor
+def S2SProcessor(*args, **kwargs):
+    return _lazy_import("neurobench.preprocessing", ".speech2spikes", "S2SProcessor")(*args, **kwargs)
+
+def MFCCProcessor(*args, **kwargs):
+    return _lazy_import("neurobench.preprocessing", ".mfcc", "MFCCProcessor")(*args, **kwargs)
--- neurobench/utils.py
+++ neurobench/utils.py
@@ -1,5 +1,7 @@
 """
 """
+from importlib import import_module
 
-def utility_function():
-    ...
+def _lazy_import(package_name, module_name, class_name):
+    module = import_module(module_name, package=package_name)
+    return getattr(module, class_name)

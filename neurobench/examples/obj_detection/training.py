70818be7b092a3868b0207d586baa66bbc78904f
korneelf1 <korneel.vandenberghe@hotmail.be>
2023-09-14 21:33:22 -0400

    altered training files

--- neurobench/examples/obj_detection/training.py
+++
@@ -1,3 +0,0 @@
-# this file should contain the code used for training the model used for the benchmark
-
-# TODO: make the training scripts available in the repo, currently they are on the imec_object_detection branch

6c13804c399247c5d5116ed2319300ace52e03e2
korneelf1 <korneel.vandenberghe@hotmail.be>
2023-09-21 15:24:07 -0400

    Ensure unique list of activation modules

--- neurobench/models/utils.py
+++ neurobench/models/utils.py
@@ -4,8 +4,8 @@ import snntorch as snn
 
 def activation_modules():
     """
-    The activation layers that can be auto-deteced.
+    The activation layers that can be auto-deteced. Every activation layer can only be included once.
     """
-    return [nn.ReLU,
+    return list(set([nn.ReLU,
             nn.Sigmoid,
-           ]
+           ]))

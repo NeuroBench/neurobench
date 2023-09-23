9610766a9d6a7e4741addf332facef79f1f29310
Jason Yik <jyik@g.harvard.edu>
2023-09-23 15:28:55 -0400

    update ANN benchmark

--- neurobench/examples/primate_reaching/benchmark.py
+++ neurobench/examples/primate_reaching/benchmark.py
@@ -1,3 +1,6 @@
+import torch
+from torch.utils.data import DataLoader, Subset
+
 from neurobench.datasets import PrimateReaching
 from neurobench.models.torch_model import TorchModel
 from neurobench.benchmarks import Benchmark
@@ -7,25 +10,25 @@ from neurobench.examples.primate_reaching.ANN import ANNModel
 # class for download instructions.
 
 # The dataloader and preprocessor has been combined together into a single class
-primate_reaching_dataset = PrimateReaching(file_path="data/primate_reaching/PrimateReachingDataset/", filename="indy_20170131_02.mat",
-                                           num_steps=7, train_ratio=0.8, mode="3D", model_type="ANN")
-test_set = primate_reaching_dataset.create_dataloader(primate_reaching_dataset.ind_test, batch_size=256, shuffle=True)
+dataset = PrimateReaching(file_path="data/primate_reaching/PrimateReachingDataset/", filename="indy_20170131_02.mat",
+                                           num_steps=7, train_ratio=0.8)
+
+test_set_loader = DataLoader(Subset(dataset, dataset.ind_test), batch_size=256, shuffle=True)
 
 
-
-net = ANNModel(input_dim=primate_reaching_dataset.input_feature_size*primate_reaching_dataset.num_steps,
+net = ANNModel(input_dim=dataset.input_feature_size*dataset.num_steps,
                layer1=32, layer2=48, output_dim=2, dropout_rate=0.5)
 
 # Give the user the option to load their pretrained weights
-# net.load_state_dict(torch.load("neurobench/examples/primate_reaching/model_data/model_parameters.pth"))
+# TODO: currently model is not trained
+# net.load_state_dict(torch.load("neurobench/examples/primate_reaching/model_data/model_parameters"))
 
 model = TorchModel(net)
 
-# metrics = ["r_squared", "model_size", "latency", "MACs"]
-static_metrics = ["model_size"]
+static_metrics = ["model_size", "connection_sparsity"]
 data_metrics = ["r2"]
 
 # Benchmark expects the following:
-benchmark = Benchmark(model, test_set, [], [], [static_metrics, data_metrics])
+benchmark = Benchmark(model, test_set_loader, [], [], [static_metrics, data_metrics])
 results = benchmark.run()
 print(results)

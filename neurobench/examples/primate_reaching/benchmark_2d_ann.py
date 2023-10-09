633cb2b267fe4bc2025fd9ada649829a1bce7c64
Jason Yik <jyik@g.harvard.edu>
2023-10-09 11:44:57 -0400

    update ANN PR benchmarks

--- neurobench/examples/primate_reaching/benchmark_2d_ann.py
+++ neurobench/examples/primate_reaching/benchmark_2d_ann.py
@@ -12,7 +12,18 @@ from neurobench.examples.primate_reaching.ANN import ANNModel2D
 all_files = ["indy_20160622_01", "indy_20160630_01", "indy_20170131_02", 
              "loco_20170210_03", "loco_20170215_02", "loco_20170301_05"]
 
+footprint = []
+connection_sparsity = []
+activation_sparsity = []
+dense = []
+macs = []
+acs = []
+r2 = []
+
+
 for filename in all_files:
+    print("Processing {}".format(filename))
+
     # The dataloader and preprocessor has been combined together into a single class
     dataset = PrimateReaching(file_path="data/primate_reaching/PrimateReachingDataset/", filename=filename,
                             num_steps=1, train_ratio=0.5, bin_width=0.004,
@@ -21,18 +32,32 @@ for filename in all_files:
     test_set_loader = DataLoader(Subset(dataset, dataset.ind_test), batch_size=256, shuffle=False)
 
     net = ANNModel2D(input_dim=dataset.input_feature_size, layer1=32, layer2=48, 
-                     output_dim=2, bin_window=0.2, dropout_rate=0.5)
+                     output_dim=2, bin_window=0.2, drop_rate=0.5)
 
-    # Give the user the option to load their pretrained weights
-    # TODO: currently model is not trained
-    net.load_state_dict(torch.load("neurobench/examples/primate_reaching/model_data/2D_ANN_Weight/"+filename+"_model_state_dict.pth"))
+    net.load_state_dict(torch.load("neurobench/examples/primate_reaching/model_data/2D_ANN_Weight/"+filename+"_model_state_dict.pth", map_location=torch.device('cpu')))
 
     model = TorchModel(net)
 
-    static_metrics = ["model_size"]
-    data_metrics = ["r2"]
+    static_metrics = ["model_size", "connection_sparsity"]
+    data_metrics = ["r2", "activation_sparsity", "synaptic_operations"]
 
     # Benchmark expects the following:
     benchmark = Benchmark(model, test_set_loader, [], [], [static_metrics, data_metrics])
     results = benchmark.run()
     print(results)
+
+    footprint.append(results['model_size'])
+    connection_sparsity.append(results['connection_sparsity'])
+    activation_sparsity.append(results['activation_sparsity'])
+    dense.append(results['synaptic_operations']['Dense'])
+    macs.append(results['synaptic_operations']['Effective_MACs'])
+    acs.append(results['synaptic_operations']['Effective_ACs'])
+    r2.append(results['r2'])
+
+print("Footprint: {}".format(footprint))
+print("Connection sparsity: {}".format(connection_sparsity))
+print("Activation sparsity: {}".format(activation_sparsity), sum(activation_sparsity)/len(activation_sparsity))
+print("Dense: {}".format(dense), sum(dense)/len(dense))
+print("MACs: {}".format(macs), sum(macs)/len(macs))
+print("ACs: {}".format(acs), sum(acs)/len(acs))
+print("R2: {}".format(r2), sum(r2)/len(r2))
--- neurobench/examples/primate_reaching/benchmark_3d_ann.py
+++ neurobench/examples/primate_reaching/benchmark_3d_ann.py
@@ -12,7 +12,17 @@ from neurobench.examples.primate_reaching.ANN import ANNModel3D
 all_files = ["indy_20160622_01", "indy_20160630_01", "indy_20170131_02", 
              "loco_20170210_03", "loco_20170215_02", "loco_20170301_05"]
 
+footprint = []
+connection_sparsity = []
+activation_sparsity = []
+dense = []
+macs = []
+acs = []
+r2 = []
+
 for filename in all_files:
+    print("Processing {}".format(filename))
+
     # The dataloader and preprocessor has been combined together into a single class
     dataset = PrimateReaching(file_path="data/primate_reaching/PrimateReachingDataset/", filename=filename,
                             num_steps=1, train_ratio=0.5, bin_width=0.004,
@@ -21,18 +31,33 @@ for filename in all_files:
     test_set_loader = DataLoader(Subset(dataset, dataset.ind_test), batch_size=256, shuffle=False)
 
     net = ANNModel3D(input_dim=dataset.input_feature_size, layer1=32, layer2=48, 
-                     output_dim=2, bin_window=0.2, num_steps=7, dropout_rate=0.5)
+                     output_dim=2, bin_window=0.2, num_steps=7, drop_rate=0.5)
 
-    # Give the user the option to load their pretrained weights
-    # TODO: currently model is not trained
     net.load_state_dict(torch.load("neurobench/examples/primate_reaching/model_data/3D_ANN_Weight/"+filename+"_model_state_dict.pth"))
 
     model = TorchModel(net)
 
-    static_metrics = ["model_size"]
-    data_metrics = ["r2"]
+    static_metrics = ["model_size", "connection_sparsity"]
+    data_metrics = ["r2", "activation_sparsity", "synaptic_operations"]
 
     # Benchmark expects the following:
     benchmark = Benchmark(model, test_set_loader, [], [], [static_metrics, data_metrics])
     results = benchmark.run()
     print(results)
+
+    footprint.append(results['model_size'])
+    connection_sparsity.append(results['connection_sparsity'])
+    activation_sparsity.append(results['activation_sparsity'])
+    dense.append(results['synaptic_operations']['Dense'])
+    macs.append(results['synaptic_operations']['MACs'])
+    acs.append(results['synaptic_operations']['ACs'])
+    r2.append(results['r2'])
+
+print("Footprint: {}".format(footprint))
+print("Connection sparsity: {}".format(connection_sparsity))
+print("Activation sparsity: {}".format(activation_sparsity), sum(activation_sparsity)/len(activation_sparsity))
+print("Dense: {}".format(dense), sum(dense)/len(dense))
+print("MACs: {}".format(macs), sum(macs)/len(macs))
+print("ACs: {}".format(acs), sum(acs)/len(acs))
+print("R2: {}".format(r2), sum(r2)/len(r2))
+

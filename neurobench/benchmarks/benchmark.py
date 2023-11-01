f105e7934e280bc5d7b7285d9b18978b917ad098
Benedetto <s247082@studenti.polito.it>
2023-11-01 10:52:09 -0700

    Add 'verbose' parameter to output results at every evaluation batch

--- neurobench/benchmarks/benchmark.py
+++ neurobench/benchmarks/benchmark.py
@@ -2,9 +2,11 @@ from tqdm import tqdm
 
 from . import static_metrics, data_metrics
 
+
 class Benchmark():
     """ Top-level benchmark class for running benchmarks.
     """
+
     def __init__(self, model, dataloader, preprocessors, postprocessors, metric_list):
         """
         Args:
@@ -16,14 +18,14 @@ class Benchmark():
                 First item is static metrics, second item is data metrics.
         """
         self.model = model
-        self.dataloader = dataloader # dataloader not dataset
+        self.dataloader = dataloader  # dataloader not dataset
         self.preprocessors = preprocessors
         self.postprocessors = postprocessors
 
         self.static_metrics = {m: getattr(static_metrics, m) for m in metric_list[0]}
         self.data_metrics = {m: getattr(data_metrics, m) for m in metric_list[1]}
 
-    def run(self):
+    def run(self, verbose=False):
         """ Runs batched evaluation of the benchmark.
 
         Currently, data metrics are accumulated via mean over the entire
@@ -33,7 +35,7 @@ class Benchmark():
             results: A dictionary of results.
         """
         print("Running benchmark")
-        
+
         # add hooks to the model
         data_metrics.detect_activations_connections(self.model)
 
@@ -44,17 +46,18 @@ class Benchmark():
 
         # Init/re-init stateful data metrics
         for m in self.data_metrics.keys():
-            if isinstance(self.data_metrics[m],type) and issubclass(self.data_metrics[m], data_metrics.AccumulatedMetric):
+            if isinstance(self.data_metrics[m], type) and issubclass(self.data_metrics[m],
+                                                                     data_metrics.AccumulatedMetric):
                 self.data_metrics[m] = self.data_metrics[m]()
             elif isinstance(self.data_metrics[m], data_metrics.AccumulatedMetric):
                 self.data_metrics[m] = self.data_metrics[m]()
 
         dataset_len = len(self.dataloader.dataset)
-        
+
         batch_num = 0
         for data in tqdm(self.dataloader, total=len(self.dataloader)):
             batch_size = data[0].size(0)
-            
+
             # convert data to tuple
             if type(data) is not tuple:
                 data = tuple(data)
@@ -66,7 +69,7 @@ class Benchmark():
             # Run model on test data
             preds = self.model(data[0])
 
-            for alg in self.postprocessors: 
+            for alg in self.postprocessors:
                 preds = alg(preds)
 
             # Data metrics
@@ -80,21 +83,29 @@ class Benchmark():
                     continue
                 # otherwise accumulate via mean
                 else:
-                    assert isinstance(v, float) or isinstance(v, int), "Data metric must return float or int to be accumulated"
+                    assert isinstance(v, float) or isinstance(v,
+                                                              int), "Data metric must return float or int to be accumulated"
                     if m not in results:
                         results[m] = v * batch_size / dataset_len
                     else:
                         results[m] += v * batch_size / dataset_len
-            
+
             # delete hook contents
             self.model.reset_hooks()
 
-            batch_num += 1
-                
+            if verbose:
+                for m in self.data_metrics.keys():
+                    if isinstance(self.data_metrics[m], data_metrics.AccumulatedMetric):
+                        results[m] = self.data_metrics[m].compute()
+                print(f'\nBatch num {batch_num}/{len(self.dataloader)}')
+                print(results)
 
-        # compute AccumulatedMetrics after all batches
-        for m in self.data_metrics.keys():
-            if isinstance(self.data_metrics[m], data_metrics.AccumulatedMetric):
-                results[m] = self.data_metrics[m].compute()
+            batch_num += 1
+
+        # compute AccumulatedMetrics after all batches if they are not calculated at every iteration
+        if not verbose:
+            for m in self.data_metrics.keys():
+                if isinstance(self.data_metrics[m], data_metrics.AccumulatedMetric):
+                    results[m] = self.data_metrics[m].compute()
 
         return results

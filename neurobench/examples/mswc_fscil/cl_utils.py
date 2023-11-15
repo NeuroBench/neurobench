82c53f01b07651dd5a83d3511730fe63175d94b3
Jason Yik <jyik@g.harvard.edu>
2023-11-15 14:06:49 -0500

    update license info

--- neurobench/examples/mswc_fscil/cl_utils.py
+++ neurobench/examples/mswc_fscil/cl_utils.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This code has been copied from https://github.com/vlomonaco/ar1-pytorch/tree/master
+# It is licensed under Creative Commons Attribution 4.0 International License:
+
+# This work is licensed under the Creative Commons Attribution 4.0 International
+# License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
 ################################################################################
 # Copyright (c) 2020. Vincenzo Lomonaco, Gabriele Graffieti, Lorenzo           #
 # Pellegrini, Davide Maltoni. All rights reserved.                             #

3d73519825967f6645d7574d5abb64d99d38313c
Jason Yik <jyik@g.harvard.edu>
2024-01-17 16:02:35 -0500

    update dvsg script

--- neurobench/examples/dvs_gesture/benchmark.py
+++ neurobench/examples/dvs_gesture/benchmark.py
@@ -16,6 +16,8 @@ test_set = DVSGesture("data/dvs_gesture/", split="testing", preprocessing="stack
 test_set_loader = DataLoader(test_set, batch_size=16, shuffle=True,drop_last=True)
 
 net = Conv_SNN()
+
+# The pre-trained model is not available, this demo loads an untrained model.
 net.load_state_dict(torch.load('neurobench/examples/dvs_gesture/model_data/DVS_SNN_untrained.pth'))
 
 ## Define model ##
@@ -25,7 +27,7 @@ model = SNNTorchModel(net)
 postprocessors = [choose_max_count]
 
 static_metrics = ["footprint", "connection_sparsity"]
-workload_metrics = ["synaptic_operations", "activation_sparsity"]
+workload_metrics = ["synaptic_operations", "activation_sparsity", "classification_accuracy"]
 
 benchmark = Benchmark(model, test_set_loader, [], postprocessors, [static_metrics, workload_metrics])
 results = benchmark.run()

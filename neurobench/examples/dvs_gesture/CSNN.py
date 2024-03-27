522287128d79cdc927475a215ae3a08f330544b7
Korneel Van den Berghe <k.c.m.vandenberghe@student.tudelft.nl>
2024-03-27 10:44:22 +0100

    initialize correctly

--- neurobench/examples/dvs_gesture/CSNN.py
+++ neurobench/examples/dvs_gesture/CSNN.py
@@ -13,6 +13,7 @@ from neurobench.postprocessing.postprocessor import choose_max_count
 
 from tqdm import tqdm
 
+import wandb
 # from torch.profiler import profile, record_function, ProfilerActivity
 
 class Conv_SNN(nn.Module):
@@ -26,25 +27,30 @@ class Conv_SNN(nn.Module):
 		stride = 2
 		self.pool1 = nn.AvgPool2d(2,stride=stride)
 		self.conv1 = nn.Conv2d(3,24,3,1)
-		self.syn1  = snn.Synaptic(alpha=alpha, beta= beta, spike_grad = grad)
+		# initialize the weights of the convolutional layer with a normal distribution with mean .5 and std 0.25
+		nn.init.normal_(self.conv1.weight, mean=0.5, std=0.25)
+
+		self.syn1  = snn.Leaky(beta= .5, spike_grad = grad)
 
 		self.pool2 = nn.AvgPool2d(2,stride=stride)
 		self.conv2 = nn.Conv2d(24,24,3, 1)
-		self.syn2  = snn.Synaptic(alpha=alpha, beta= beta, spike_grad = grad)
+		nn.init.normal_(self.conv2.weight, mean=0.5, std=0.25)
+		self.syn2  = snn.Leaky(beta= .7, spike_grad = grad)
 
 		self.pool3 = nn.AvgPool2d(2,stride=stride)
 		self.conv3 = nn.Conv2d(24, 64,3,1)
-		self.syn3  = snn.Synaptic(alpha=alpha, beta= beta, spike_grad = grad)
+		nn.init.normal_(self.conv3.weight, mean=0.5, std=0.25)
+		self.syn3  = snn.Leaky(beta= .8, spike_grad = grad)
 
 		self.lin1  = nn.Linear(1024,11)
-		self.syn4  = snn.Synaptic(alpha=alpha, beta= beta, spike_grad = grad)
+		self.syn4  = snn.Leaky(beta= .95, spike_grad = grad)
 
 		self.lin2  = nn.Linear(128,11)
 
-		self.mem1, self.cur1 = self.syn1.init_synaptic()
-		self.mem2, self.cur2 = self.syn2.init_synaptic()
-		self.mem3, self.cur3 = self.syn3.init_synaptic()
-		self.mem4, self.cur4 = self.syn4.init_synaptic()
+		self.mem1 = self.syn1.init_leaky()
+		self.mem2 = self.syn2.init_leaky()
+		self.mem3 = self.syn3.init_leaky()
+		self.mem4 = self.syn4.init_leaky()
 
 
 	def forward(self, frame, warmup_frames = 0):
@@ -54,33 +60,37 @@ class Conv_SNN(nn.Module):
 		x = self.conv1(frame)
 		x = self.pool1(x)
 		# print(x.shape)
-		x, self.mem1, self.cur1 = self.syn1(x, self.mem1,self.cur1)
+		x, self.mem1 = self.syn1(x, self.mem1)
 		
 		x = self.conv2(x)
 		x = self.pool2(x)
-		x, self.mem2, self.cur2 = self.syn2(x, self.mem2,self.cur2)
+		x, self.mem2 = self.syn2(x, self.mem2)
 
 		x = self.conv3(x)
 		
-		x, self.mem3, self.cur3 = self.syn3(x, self.mem3,self.cur3)
+		x, self.mem3 = self.syn3(x, self.mem3)
 
 		x = x.view(x.shape[0],-1)
 		x = self.lin1(x)
 		
-		spks, self.mem4, self.cur4 = self.syn4(x, self.mem4,self.cur4)
+		spks, self.mem4 = self.syn4(x, self.mem4)
 		return spks.reshape(-1,11).detach(), self.mem4
 	def reset(self):
-		self.mem1, self.cur1 = self.syn1.init_synaptic()
-		self.mem2, self.cur2 = self.syn2.init_synaptic()
-		self.mem3, self.cur3 = self.syn3.init_synaptic()
-		self.mem4, self.cur4 = self.syn4.init_synaptic()
+		self.mem1 = self.syn1.init_leaky()
+		self.mem2 = self.syn2.init_leaky()
+		self.mem3 = self.syn3.init_leaky()
+		self.mem4 = self.syn4.init_leaky()
 
-	def single_forward(self, frames, warmup_frames = 0):
+	def single_forward(self, frames, warmup_frames = 0, wandb_log = False):
 		self.reset()
 
 		out_spk = 0
 		# from [nr_batches,nr_frames,c,h,w] -> [nr_frames,nr_batches,c,h,w]
 		# frames = frames.transpose(1,0)
+		spk_1 = []
+		spk_2 = []
+		spk_3 = []
+		spk_4 = []
 
 		# Data is expected to be shape (batch, timestep, features*)
 		for step in range(frames.shape[1]):
@@ -90,32 +100,70 @@ class Conv_SNN(nn.Module):
 			# frame = transforms.Resize((32,32))(frame).to(dtype=torch.float32)
 			x = self.conv1(frame)
 			x = self.pool1(x)
-			x, self.mem1, self.cur1 = self.syn1(x, self.mem1,self.cur1)
+			x, self.mem1 = self.syn1(x, self.mem1)
+			spk_1.append(x)
+			if wandb_log:
+				# wandb.log({'spikes1':x})
+				# average number of spikes in layer 1
+				spikes_avg = torch.mean(x)
+				wandb.log({"avg membrane potential layer 1 in last training round" : torch.mean(self.mem1).item()})
+				wandb.log({"avg nr of spikes layer 1 in last training round" : spikes_avg.item()})
+
+				# Log y with WandB
+				# wandb.log({f'spikes_1': [(step, y.item())]})
 			x = self.conv2(x)
 			x = self.pool2(x)
-			x, self.mem2, self.cur2 = self.syn2(x, self.mem2,self.cur2)
+			x, self.mem2 = self.syn2(x, self.mem2)
+			spk_2.append(x)
+			if wandb_log:
+				spikes_avg = torch.mean(x)
+				wandb.log({"avg nr of spikes layer 2 in last training round" : spikes_avg.item()})
 			x = self.conv3(x)
 			
-			x, self.mem3, self.cur3 = self.syn3(x, self.mem3,self.cur3)
+			x, self.mem3 = self.syn3(x, self.mem3)
+			spk_3.append(x)
+			if wandb_log:
+				spikes_avg = torch.mean(x)
+				wandb.log({"avg nr of spikes layer 3 in last training round" : spikes_avg.item()})
 			x = x.view(x.shape[0],-1)
 			x = self.lin1(x)
 			
-			x, self.mem4, self.cur4 = self.syn4(x, self.mem4,self.cur4)
+			x, self.mem4 = self.syn4(x, self.mem4)
+			spk_4.append(x)
+			if wandb_log:
+				spikes_avg = torch.mean(x)
+				wandb.log({"avg nr of spikes layer 4 in last training round" : spikes_avg.item()})
 
 			if step >= warmup_frames:
 				out_spk += x
 
+		# log average spike rates of each layer
+		spk_1 = torch.stack(spk_1)
+		spk_2 = torch.stack(spk_2)
+		spk_3 = torch.stack(spk_3)
+		spk_4 = torch.stack(spk_4)
 
+		wandb.log({'average spike rate of layer 1': torch.mean(spk_1)})
+		wandb.log({'average spike rate of layer 1': torch.mean(spk_2)})
+		wandb.log({'average spike rate of layer 3': torch.mean(spk_3)})
+		wandb.log({'average spike rate of layer 4': torch.mean(spk_4)})
+		# log a table of the output spikes with wandb
+		
+		# print(out_spk)
 		prediction = torch.nn.functional.softmax(out_spk.reshape(11,-1), dim=0)
 		return prediction
 	
 	def fit(self, dataloader_training, warmup_frames, optimizer, device, nr_episodes = 10):
-		for _ in tqdm(range(nr_episodes)):
+		for episode in tqdm(range(nr_episodes)):
+			# print(f"Episode {episode}")
 			for frames, labels in dataloader_training:
 				# Print GPU memory
 				# with profile(activities=[ProfilerActivity.CPU], record_shapes=True, profile_memory=True) as prof:
 					# with record_function("model_inference"):
-				prediction = self.single_forward(frames.to(device), warmup_frames)
+				wandb_log = False
+				if episode == nr_episodes-1:
+					wandb_log = True
+				prediction = self.single_forward(frames.to(device), warmup_frames, wandb_log=wandb_log)
 
 				# Add a delay to allow profiler to collect data
 				# import time
@@ -128,10 +176,14 @@ class Conv_SNN(nn.Module):
 				targets_one_hot = torch.nn.functional.one_hot(label_tensor, num_classes=11).transpose(1,0)
 
 				loss = torch.nn.functional.smooth_l1_loss(prediction,targets_one_hot)
+				
+
+
 				optimizer.zero_grad()
 				loss.backward()
+				wandb.log({'loss': loss.item()})
 				optimizer.step()
-			print(loss.item())
+			# print(loss.item())
 
 
 
--- neurobench/examples/dvs_gesture/training.py
+++ neurobench/examples/dvs_gesture/training.py
@@ -13,19 +13,40 @@ from neurobench.postprocessing.postprocessor import aggregate,choose_max_count
 
 from CSNN import Conv_SNN
 
+#wandb tracking
+import wandb
+
+run = wandb.init(project='dvs_gesture', 
+        #    entity='neurobench',
+           config={
+               'model': 'CSNN',
+               'dataset': 'dvs_gesture',
+               'batch_size': 1,
+               'learning_rate': 1.2e-3,
+               'warmup_frames': 34,
+               'nr_episodes': 10,
+               'optimizer': 'Adamax',
+               'betas': [0.9, 0.95]
+           })
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = 'cpu'
-
+# Set the seeds for reproducibility
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
 # data in repo root dir
 data = DVSGesture('../../../data/dvs_gesture/', split='testing', preprocessing='stack')
-dataloader_training = DataLoader(data, 10,shuffle=False)
+dataloader_training = DataLoader(data, 24,shuffle=False)
 model = Conv_SNN().to(device)
-data_1 = [(torch.tensor(data[0][0]).unsqueeze(0),torch.tensor(data[0][1]).unsqueeze(0))]
-data_2 = [next(iter(dataloader_training))]
+run.watch(model, log='all', log_freq=1)
+# data_1 = [(torch.tensor(data[0][0]).unsqueeze(0),torch.tensor(data[0][1]).unsqueeze(0))]
+# data_2 = [next(iter(dataloader_training))]
 
-torch.save(model.state_dict(), 'model_data/DVS_SNN_untrained.pth')
+# single_batch = next(iter(dataloader_training))
+# print(single_batch[0].shape)
+# torch.save(model.state_dict(), 'model_data/DVS_SNN_untrained.pth')
 
 optimizer = torch.optim.Adamax(model.parameters(),lr=1.2e-3,betas=[0.9,0.95])
 # model.fit(dataloader_training=dataloader_training,device=device, warmup_frames=70, optimizer=optimizer, nr_episodes=1000)
-# model.fit(dataloader_training=data_2,device=device, warmup_frames=70, optimizer=optimizer, nr_episodes=10)
+model.fit(dataloader_training=dataloader_training,device=device, warmup_frames=34, optimizer=optimizer, nr_episodes=3)
 # torch.save(model.state_dict(), 'neurobench/examples/dvs_gesture/model_data/DVS_SNN_trained.pth')
